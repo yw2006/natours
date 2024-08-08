8d5a7e3bcde987ec0cbbfc4dc285ec4039e00d8b
yousef_walid <yw012276@gmail.com>
2024-08-08 14:01:16 +0300

    start create tournament api

---
+++ server.js
@@ -0,0 +1,13 @@
+const mongoose= require('mongoose');
+const dotenv = require('dotenv');
+const app = require('./app');
+
+dotenv.config();
+
+const DB=process.env.DATABASE.replace("<PASSWORD>",process.env.DATABASE_PASSWORD)
+mongoose.connect(DB)
+  .then(() => console.log('db connection successfull!👌'));
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`App running on port ${port}...`);
+});
